9332b341fcd4ea7b1966014a4fd74c4343781d10
mariodevbash <mario120gdl@gmail.com>
2021-12-30 18:18:46 -0600

    Primer Push

---
+++ src/modules/ui/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-menu mode="horizontal">
+    <a-menu-item key="personajes">
+      <router-link :to="{ name: 'Home' }" active-class="red">
+        <ion-icon name="people-outline"></ion-icon>
+        Personajes
+      </router-link>
+    </a-menu-item>
+    <a-menu-item key="about">
+      <router-link :to="{ name: 'About' }" active-class="red">
+        <ion-icon name="search-outline"></ion-icon>
+        Buscar Personaje
+      </router-link>
+    </a-menu-item>
+  </a-menu>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useRoute } from 'vue-router'
+export default defineComponent({
+  setup() {
+    const route = useRoute()
+    const current = ref([route.path]);
+    return {
+      current,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.red{
+    color: var(--main-color);
+}
+</style>
